78cc99b96255ca2d884fc22de66581a3fb4395a6
Riley Ittidecharchoti <polyglotpengyou@gmail.com>
2021-07-25 11:28:00 -0700

    stuff

--- basic_tutorial_stuff/experiments.js
+++ basic_tutorial_stuff/experiments.js
@@ -30,3 +30,17 @@ while (friendsAtSchool < 10) {
   friendsAtSchool += 1;
 }
 console.log(friendsAtSchool);
+
+const car = {
+  wheels:4,
+  color: "red",
+  drive: function(){
+    console.log("vroom vroom")
+  }
+}
+console.log(Object.keys(car)[0])
+console.log(typeof Object.keys(car)[0])
+car.drive();
+car.color = "blue";
+console.log(car.color);
+console.log(Object.values(car)[1]);
